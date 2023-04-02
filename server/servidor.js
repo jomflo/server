d9895407833a2f1501a4e8122826087a9ddfa015
unknown <you@example.com>
2023-04-01 18:49:06 -0600

    mi segundo commit

--- server/servidor.js
+++ server/servidor.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require('dotenv').config({ path: './server/.env' });
+require('dotenv').config({ path: '/app/.env' });
 // require('dotenv').config();
 
 
